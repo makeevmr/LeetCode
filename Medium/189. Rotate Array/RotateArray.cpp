fe24426ab3e35e1850d49337a0e8aaaa1d5b250b
makeevmr <art.makeev2016@yandex.ru>
2023-09-11 21:26:53 +0300

    Problem 189 solved

--- Medium/189. Rotate Array/RotateArray.cpp
+++ Medium/189. Rotate Array/RotateArray.cpp
@@ -1,22 +1,19 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 class Solution {
 public:
     static void rotate(std::vector<int> &nums, int k) {
-        size_t nums_size = nums.size();
-        size_t rotates = static_cast<size_t>(k) % nums_size;
-        for (size_t i = nums_size - rotates; i < nums_size; ++i) {
-            int end_swap_index = static_cast<int>(i + rotates - nums_size);
-            for (int j = i - 1; j >= end_swap_index; --j) {
-                std::swap(nums[j], nums[j + 1]);
-            }
-        }
+        int rotates = k % static_cast<int>(nums.size());
+        std::reverse(nums.begin(), nums.end());
+        std::reverse(nums.begin(), nums.begin() + rotates);
+        std::reverse(nums.begin() + rotates, nums.end());
     }
 };
 
 int main() {
-    std::vector<int> nums = {-1, -100, 3, 99};
+    std::vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
     Solution::rotate(nums, 2);
     for (size_t i = 0; i < nums.size(); ++i) {
         std::cout << nums[i] << ' ';
