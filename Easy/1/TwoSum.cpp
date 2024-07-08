4d25398573b6836110004792b0a57c3d19457044
makeevmr <art.makeev2016@yandex.ru>
2024-07-08 15:27:56 +0300

    Problem 1 solved

---
+++ Easy/1/TwoSum.cpp
@@ -0,0 +1,48 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such
+that they add up to target. You may assume that each input would have exactly one solution, and you
+may not use the same element twice. You can return the answer in any order.
+Time complexity: O(n), where n is size of nums
+Additional memory complexity: O(n)
+Idea: use unordered_map to store all possible values and their indexes, iterate through nums and, if
+value target - nums[i] in map, return pair of indexes
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+class Solution {
+public:
+    static std::vector<int> twoSum(const std::vector<int> &nums, int target) {
+        std::vector<int> index_pair = {-1, -1};
+        std::unordered_map<int, int> first_occurrences = {{nums[0], 0}};
+        std::size_t nums_size = nums.size();
+        for (std::size_t i = 1; i < nums_size; ++i) {
+            if (auto f_iter = first_occurrences.find(target - nums[i]);
+                f_iter != first_occurrences.end()) {
+                index_pair[0] = f_iter->second;
+                index_pair[1] = static_cast<int>(i);
+                break;
+            }
+            if (auto f_iter = first_occurrences.find(nums[i]); f_iter == first_occurrences.end()) {
+                first_occurrences.insert({nums[i], i});
+            }
+        }
+        return index_pair;
+    }
+};
+
+int main() {
+    // std::vector<int> nums = {2, 7, 11, 15};
+    // int target = 9;
+    // std::vector<int> nums = {3, 2, 4};
+    // int target = 6;
+    std::vector<int> nums = {3, 3};
+    int target = 6;
+    std::vector<int> answer = Solution::twoSum(nums, target);
+    for (const auto &numb : answer) {
+        std::cout << numb << ' ';
+    }
+    std::cout << '\n';
+    return 0;
+}
