e4662fbfa4bceed9850b38674f800b240d9d414f
makeevmr <art.makeev2016@yandex.ru>
2024-09-20 08:49:34 +0300

    Problem 191 solved

---
+++ Easy/191/Number1Bits.cpp
@@ -0,0 +1,13 @@
+/* Write a function that takes the binary representation of a positive integer and returns the
+number of set bits it has (also known as the Hamming weight). */
+
+class Solution {
+public:
+    [[nodiscard]] static int hammingWeight(int n) {
+        int set_bits_counter = 0;
+        for (int i = 0; i < 32; ++i) {
+            set_bits_counter += (n >> i) & 1;
+        }
+        return set_bits_counter;
+    }
+};
