01ad48e4146a5e9e063cd654516138e7a28e071e
makeevmr <art.makeev2016@yandex.ru>
2024-09-24 23:39:49 +0300

    Problem 50 solved

---
+++ Medium/50/Pow.cpp
@@ -0,0 +1,24 @@
+/* Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+Time complexity: O(log(n))
+Additional memeory complexity: O(1)
+Idea: use binary exponentiation
+*/
+
+#include <cmath>
+
+class Solution {
+public:
+    [[nodiscard]] static double myPow(double x, int n) {
+        bool is_pos_pow = n > 0;
+        int pos_pow = std::abs(n);
+        double answer = 1.0;
+        while (pos_pow > 0) {
+            if (static_cast<bool>(pos_pow & 1)) {
+                answer *= x;
+            }
+            pos_pow >>= 1;
+            x *= x;
+        }
+        return is_pos_pow ? answer : 1.0 / answer;
+    }
+};
