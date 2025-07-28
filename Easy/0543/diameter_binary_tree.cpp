4031ffec269ca0a51340a005d3f261e16d65581c
makeevmr <art.makeev2016@yandex.ru>
2025-07-29 00:13:51 +0300

    fix: problem 543 fixed

--- Easy/0543/diameter_binary_tree.cpp
+++ Easy/0543/diameter_binary_tree.cpp
@@ -1,4 +1,29 @@
-// Given the root of a binary tree, return the length of the diameter of the tree.
+// Given the root of a binary tree, return the length of the diameter of the
+// tree.
+
+#include <utility>
+#include <algorithm>
+
+struct TreeNode {
+    int val;
+    const TreeNode* left;
+    const TreeNode* right;
+
+    TreeNode()
+        : val(0),
+          left(nullptr),
+          right(nullptr) {}
+
+    explicit TreeNode(const int& val)
+        : val(val),
+          left(nullptr),
+          right(nullptr) {}
+
+    TreeNode(const int& val, const TreeNode* left, const TreeNode* right)
+        : val(val),
+          left(left),
+          right(right) {}
+};
 
 class Solution {
 public:
@@ -7,7 +32,8 @@ public:
     }
 
 private:
-    [[nodiscard]] static std::pair<int, int> getSubtreeInfo(const TreeNode* root) {
+    [[nodiscard]] static std::pair<int, int> getSubtreeInfo(
+        const TreeNode* root) {
         std::pair<int, int> left_subtree_info = {-1, -1};
         std::pair<int, int> right_subtree_info = {-1, -1};
         if (root->left != nullptr) {
@@ -16,8 +42,11 @@ private:
         if (root->right != nullptr) {
             right_subtree_info = getSubtreeInfo(root->right);
         }
-        int max_path = std::max(left_subtree_info.first, right_subtree_info.first) + 1;
-        int diameter = std::max(std::max(left_subtree_info.second, right_subtree_info.second), left_subtree_info.first + right_subtree_info.first + 2);
+        int max_path =
+            std::max(left_subtree_info.first, right_subtree_info.first) + 1;
+        int diameter = std::max(
+            std::max(left_subtree_info.second, right_subtree_info.second),
+            left_subtree_info.first + right_subtree_info.first + 2);
         return std::pair<int, int>{max_path, diameter};
     }
 };
