68f924345247901c3dc9766f8148476193ddaddc
makeevmr <art.makeev2016@yandex.ru>
2025-07-29 17:17:09 +0300

    fix: 118 reformatted

--- Easy/0118/pascal_triangle.cpp
+++ Easy/0118/pascal_triangle.cpp
@@ -12,8 +12,10 @@ public:
         std::vector<std::vector<int>> pascal_triangle = {std::vector<int>{1}};
         for (int _ = 1; _ < num_rows; ++_) {
             std::vector<int> new_row = {1};
-            for (std::size_t i = 0; i < pascal_triangle.back().size() - 1; ++i) {
-                new_row.push_back(pascal_triangle.back()[i] + pascal_triangle.back()[i + 1]);
+            for (std::size_t i = 0; i < pascal_triangle.back().size() - 1;
+                 ++i) {
+                new_row.push_back(pascal_triangle.back()[i] +
+                                  pascal_triangle.back()[i + 1]);
             }
             new_row.push_back(1);
             pascal_triangle.push_back(std::move(new_row));
