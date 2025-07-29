180f847fa1b831e8628695a976cacc22b0a688bf
makeevmr <art.makeev2016@yandex.ru>
2025-07-29 16:47:23 +0300

    feat: problem 152 solved

---
+++ Medium/0152/maximum_product_subarray.cpp
@@ -0,0 +1,40 @@
+/* Given an integer array nums, find a subarray that has the largest product,
+and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer. */
+
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+class Solution {
+public:
+    [[nodiscard]] static int maxProduct(const std::vector<int>& nums) {
+        int max_product = std::numeric_limits<int>::min();
+        int max_negative_prefix_ind = -1;
+        int max_negative_prefix_value = -1;
+        int curr_prefix = 1;
+        for (std::size_t i = 0; i < nums.size(); ++i) {
+            curr_prefix *= nums[i];
+            if (curr_prefix > 0) {
+                max_product = std::max(max_product, curr_prefix);
+            } else if (curr_prefix == 0) {
+                max_product = std::max(max_product, nums[i]);
+                max_negative_prefix_ind = -1;
+                max_negative_prefix_value = -1;
+                curr_prefix = 1;
+            } else {
+                int new_product = -1;
+                if (max_negative_prefix_ind != -1) {
+                    new_product = curr_prefix / max_negative_prefix_value;
+                } else {
+                    new_product = nums[i];
+                    max_negative_prefix_ind = i;
+                    max_negative_prefix_value = curr_prefix;
+                }
+                max_product = std::max(max_product, new_product);
+            }
+        }
+        return max_product;
+    }
+};
