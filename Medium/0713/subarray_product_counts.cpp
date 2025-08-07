af9a8fe05326fedc78f3f78794c26dfe3fc512f5
makeevmr <art.makeev2016@yandex.ru>
2025-08-07 04:17:11 +0300

    feat: problem 713 solved

---
+++ Medium/0713/subarray_product_counts.cpp
@@ -0,0 +1,25 @@
+/* Given an array of integers nums and an integer k, return the number of
+ * contiguous subarrays where the product of all the elements in the subarray is
+ * strictly less than k. */
+
+#include <vector>
+
+class Solution {
+public:
+    static int numSubarrayProductLessThanK(const std::vector<int>& nums,
+                                           int k) {
+        int nums_size = static_cast<int>(nums.size());
+        int left = -1;
+        int subarray_product = 1;
+        int counter = 0;
+        for (int right = 0; right < nums_size; ++right) {
+            subarray_product *= nums[right];
+            while ((left < right) && (subarray_product >= k)) {
+                ++left;
+                subarray_product /= nums[left];
+            }
+            counter += right - left;
+        }
+        return counter;
+    }
+};
